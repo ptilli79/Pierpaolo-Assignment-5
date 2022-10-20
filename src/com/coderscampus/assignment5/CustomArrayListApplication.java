82b5621bf9ca3c4833e916d065301527ce62dca5
Pierpaolo Tilli <pierpaolo.tilli@gmail.com>
2022-10-20 13:07:55 -0400

    Comment java.util.ArrayList and java.util.List labraries

--- src/com/coderscampus/assignment5/CustomArrayListApplication.java
+++ src/com/coderscampus/assignment5/CustomArrayListApplication.java
@@ -1,7 +1,7 @@
 package com.coderscampus.assignment5;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 
 public class CustomArrayListApplication {
